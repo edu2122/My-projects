86676a906be053093726c230feaed6f663fe60b3
Eduardo <eduardorafaellm@gmail.com>
2024-01-26 11:48:26 -0400

    Update opacity for completed items in Todo component

--- projects/todo-app/src/components/Todo/index.jsx
+++ projects/todo-app/src/components/Todo/index.jsx
@@ -63,14 +63,15 @@ export function Todo({ id, title, completed, isEditing, setIsEditing }) {
           <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"></path>
         </svg>
       </label>
-        <label
-          className={`
+      <label
+        className={`
               todo-label 
               ${className}`}
-        >
-          {title}
-        </label>
+      >
+        {title}
+      </label>
       <button
+        aria-label="Btn Delete todo"
         onClick={() => removeTodo({ id })}
         className={`bin-button ${className}`}
       >
